6e5c3ca7c6a3d7895bc6bdf9da412fe120968e14
Todd <toddmoulder88@gmail.com>
2016-04-13 17:03:23 -0500

    Brought in StandardAssets for Fire Particle Work.

---
+++ Zambio/Assets/Code/Enemies/HeadLocation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadLocation : MonoBehaviour {
+    public Transform headTransform;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
