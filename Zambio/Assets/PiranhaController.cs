c6d0c9338474f5b4ce3868ee22d845906f499611
zach <zsmit22@lsu.edu>
2016-04-29 00:53:59 -0500

    cleaned up some code "T" no longer shows credits Edited terain and invis walls Placeholder piranha plants. See Pipes.

---
+++ Zambio/Assets/PiranhaController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PiranhaController : MonoBehaviour {
+
+    EnemyController ec;
+
+    // Use this for initialization
+    void Start () {
+        ec = GetComponent<EnemyController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (ec.health <= 0)
+        {
+            Die();
+        }
+        print(ec.health);
+    }
+
+    public void Die()
+    {
+        ec.randomDrop();
+        Destroy(ec.gameObject);
+    }
+
+}
